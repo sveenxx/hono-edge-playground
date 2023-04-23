e4c685c1acf5e0a2f68305962fb49c6667a1b72d
sveenxx <sveenv2@gmail.com>
2023-04-23 13:43:26 +0200

    x

--- api/edge.ts
+++ api/edge.ts
@@ -1,6 +1,7 @@
 import type { RequestContext } from "@vercel/edge";
 
 import { Hono } from "hono";
+import { handle } from "hono/nextjs";
 
 export const config = {
   runtime: "edge",
@@ -9,7 +10,10 @@ export const config = {
 
 const app = new Hono();
 
-export default async (req: Request, ctx: RequestContext) => {
-  app.get("/", (c) => c.text("Hello Fastly!"));
-  app.fire();
-};
+app.get("/hello", (c) => {
+  return c.json({
+    message: "Hello Next.js!",
+  });
+});
+
+export default handle(app);
